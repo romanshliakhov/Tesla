e48e4126d3fdeb76435fc57a2ce29a41624756c9
romanshliakhov <romanshliakhov@gmail.com>
2021-04-18 00:59:30 +0300

    added latest section content

--- source/js/common.js
+++ source/js/common.js
@@ -1,4 +1,23 @@
+$(function(){
+    $('.stock__slider-inner').slick({
+		prevArrow: '.stock__arrow-prev',        
+		nextArrow: '.stock__arrow-next',				       
+        autoplay: false,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+        	  
+    });   
+});
 
+$(function(){
+    $('.latest__slider-inner').slick({
+		prevArrow: '.latest__arrow-prev',        
+		nextArrow: '.latest__arrow-next',	        		       
+        autoplay: false,
+		slidesToShow: 3,
+		slidesToScroll: 1,        	  
+    });   
+});
 
 
 
