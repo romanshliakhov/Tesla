231f2ac219bc520c3fd11d078f8c374bdc39733c
romanshliakhov <romanshliakhov@gmail.com>
2021-04-18 18:12:50 +0300

    added galery page content

--- source/js/common.js
+++ source/js/common.js
@@ -34,7 +34,18 @@ $(function(){
     });   
 });
 
-
+// tabs start
+$(function () {	
+	$('.galery__tab').on('click', function(e){
+	  e.preventDefault();
+	  $('.galery__tab').removeClass('galery__tab-active');
+	  $('.galery__content').removeClass('galery__tab--active');
+  
+	  $(this).addClass('galery__tab-active');
+	  $($(this).attr('href')).addClass('galery__tab--active');
+	});	
+  });
+// tabs end
 
 
 
