99c35ef8521517c23a49ad275f3ac2f1a15ea1be
romanshliakhov <romanshliakhov@gmail.com>
2021-05-05 10:52:34 +0300

    updated galery

--- source/js/common.js
+++ source/js/common.js
@@ -287,7 +287,17 @@ $('.parts__mob-header').click(function () {
 })
 // accordion end
 
-var child = $('.main-nav__child');
+// menu overlay 
+$('.border__block').hover(function () {
+	$(this).prev().toggleClass('border__block--none')
+})
+
+$('.menu__main').hover(function () {
+	$('.overlay').toggleClass('overlay--display')
+});
+
+// 
+const child = $('.main-nav__child');
 
 $(child).hover(function () {
 	$(this).toggleClass('main-nav__child--active');
@@ -299,61 +309,59 @@ lightGallery(document.getElementById('aniimated-thumbnials'), {
 	fullScreen: true,
 });
 
-$(".polzunok-5").slider({
-	min: 0,
-	max: 100000,
-	values: [15000, 80000],
-	range: true,
-	animate: "fast",
-	slide: function (event, ui) {
-		$(".polzunok-input-5-left").val(ui.values[0]);
-		$(".polzunok-input-5-right").val(ui.values[1]);
-	}
-});
-
-$(".polzunok-input-5-left").val($(".polzunok-5").slider("values", 0));
-$(".polzunok-input-5-right").val($(".polzunok-5").slider("values", 1));
-$(".polzunok-container-5 input").change(function () {
-	var input_left = $(".polzunok-input-5-left").val().replace(/[^0-9]/g, ''),
-		opt_left = $(".polzunok-5").slider("option", "min"),
-		where_right = $(".polzunok-5").slider("values", 1),
-		input_right = $(".polzunok-input-5-right").val().replace(/[^0-9]/g, ''),
-		opt_right = $(".polzunok-5").slider("option", "max"),
-		where_left = $(".polzunok-5").slider("values", 0);
-	if (input_left > where_right) {
-		input_left = where_right;
-	}
-	if (input_left < opt_left) {
-		input_left = opt_left;
-	}
-	if (input_left == "") {
-		input_left = 0;
-	}
-	if (input_right < where_left) {
-		input_right = where_left;
-	}
-	if (input_right > opt_right) {
-		input_right = opt_right;
-	}
-	if (input_right == "") {
-		input_right = 0;
-	}
-	$(".polzunok-input-5-left").val(input_left);
-	$(".polzunok-input-5-right").val(input_right);
-	if (input_left != where_left) {
-		$(".polzunok-5").slider("values", 0, input_left);
-	}
-	if (input_right != where_right) {
-		$(".polzunok-5").slider("values", 1, input_right);
-	}
-});
-// polzunok end 
 
 
 
+// range slider start
 
+// $(".polzunok-5").slider({
+// 	min: 0,
+// 	max: 100000,
+// 	values: [15000, 80000],
+// 	range: true,
+// 	animate: "fast",
+// 	slide: function (event, ui) {
+// 		$(".polzunok-input-5-left").val(ui.values[0]);
+// 		$(".polzunok-input-5-right").val(ui.values[1]);
+// 	}
+// });
 
-
+// $(".polzunok-input-5-left").val($(".polzunok-5").slider("values", 0));
+// $(".polzunok-input-5-right").val($(".polzunok-5").slider("values", 1));
+// $(".polzunok-container-5 input").change(function () {
+// 	var input_left = $(".polzunok-input-5-left").val().replace(/[^0-9]/g, ''),
+// 		opt_left = $(".polzunok-5").slider("option", "min"),
+// 		where_right = $(".polzunok-5").slider("values", 1),
+// 		input_right = $(".polzunok-input-5-right").val().replace(/[^0-9]/g, ''),
+// 		opt_right = $(".polzunok-5").slider("option", "max"),
+// 		where_left = $(".polzunok-5").slider("values", 0);
+// 	if (input_left > where_right) {
+// 		input_left = where_right;
+// 	}
+// 	if (input_left < opt_left) {
+// 		input_left = opt_left;
+// 	}
+// 	if (input_left == "") {
+// 		input_left = 0;
+// 	}
+// 	if (input_right < where_left) {
+// 		input_right = where_left;
+// 	}
+// 	if (input_right > opt_right) {
+// 		input_right = opt_right;
+// 	}
+// 	if (input_right == "") {
+// 		input_right = 0;
+// 	}
+// 	$(".polzunok-input-5-left").val(input_left);
+// 	$(".polzunok-input-5-right").val(input_right);
+// 	if (input_left != where_left) {
+// 		$(".polzunok-5").slider("values", 0, input_left);
+// 	}
+// 	if (input_right != where_right) {
+// 		$(".polzunok-5").slider("values", 1, input_right);
+// 	}
+// });
 
 
 
@@ -361,14 +369,14 @@ $(".polzunok-container-5 input").change(function () {
 
 
 
-// menu overlay 
-$('.border__block').hover(function () {
-	$(this).prev().toggleClass('border__block--none')
-})
 
-$('.menu__main').hover(function () {
-	$('.overlay').toggleClass('overlay--display')
-});
+
+
+
+
+
+
+
 
 
 
